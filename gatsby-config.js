9ccd2e5e7d4881b553dd99aa7eb6b26de95f6427
Henrik Nygren <nygrenh@gmail.com>
2018-12-07 13:04:21 +0200

    Add Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Ohjelmoinnin MOOC 2019',
+    title: "Ohjelmoinnin MOOC 2019"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/data`,
-        name: 'markdown-pages',
-      },
+        name: "markdown-pages"
+      }
     },
     `gatsby-plugin-sharp`,
     `gatsby-plugin-catch-links`,
@@ -20,13 +20,13 @@ module.exports = {
         plugins: [
           `gatsby-remark-copy-linked-files`,
           {
-            resolve: 'gatsby-remark-smartypants',
+            resolve: "gatsby-remark-smartypants",
             options: {
               quotes: false,
               ellipses: false,
               backticks: false,
-              dashes: 'inverted'
-            },
+              dashes: "inverted"
+            }
           },
           {
             resolve: `gatsby-remark-prismjs`,
@@ -38,7 +38,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
+              classPrefix: "language-",
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
@@ -59,8 +59,8 @@ module.exports = {
               showLineNumbers: false,
               // If setting this to true, the parser won't handle and highlight inline
               // code used in markdown i.e. single backtick code like `this`.
-              noInlineHighlight: false,
-            },
+              noInlineHighlight: false
+            }
           },
           {
             resolve: `gatsby-remark-images`,
@@ -69,39 +69,49 @@ module.exports = {
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 920,
-              withWebp: true,
-            },
+              withWebp: true
+            }
           },
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'noopener noreferrer',
-            },
+              target: "_blank",
+              rel: "noopener noreferrer"
+            }
           },
           {
-            resolve: 'gatsby-remark-emojis',
+            resolve: "gatsby-remark-emojis",
             options: {
               // Deactivate the plugin globally (default: true)
               active: true,
               // Add a custom css class
-              class: 'emoji-icon',
+              class: "emoji-icon",
               // Select the size (available size: 16, 24, 32, 64)
               size: 64,
               // Add custom styles
               styles: {
-                display: 'inline',
-                margin: '0',
-                'margin-top': '1px',
-                position: 'relative',
-                top: '5px',
-                width: '25px',
-              },
-            },
+                display: "inline",
+                margin: "0",
+                "margin-top": "1px",
+                position: "relative",
+                top: "5px",
+                width: "25px"
+              }
+            }
           },
-          `gatsby-remark-component`,
-        ],
-      },
+          `gatsby-remark-component`
+        ]
+      }
     },
-  ],
-}
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-47575342-9",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true
+      }
+    }
+  ]
+};
