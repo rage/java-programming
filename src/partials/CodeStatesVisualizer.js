769b6f49be88a228feeb8c7a7781987aa4d1becc
Henrik Nygren <nygrenh@gmail.com>
2018-12-17 18:10:10 +0200

    Only render code states visualizer in the browser

--- src/partials/CodeStatesVisualizer.js
+++ src/partials/CodeStatesVisualizer.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import CodeStatesVisualizer from 'code-states-visualizer'
+import React, { lazy } from 'react'
+
 import 'code-states-visualizer/dist/app.css'
+const CodeStatesVisualizer = lazy(() => import('code-states-visualizer'))
 
 export default class CodeStatesVisualizerWrapper extends React.Component {
+  state = {
+    render: false,
+  }
+
+  componentDidMount() {
+    this.setState({ render: true })
+  }
+
   render() {
+    if (!this.state.render) {
+      return <div>Loading...</div>
+    }
     const { input } = this.props
     return <CodeStatesVisualizer input={input} />
   }
