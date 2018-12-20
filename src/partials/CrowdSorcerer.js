fda9ee6d96ce6de5b04f2cc1a927c455832e23b3
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-12-20 15:21:43 +0200

    Add CrowdSorcerer

---
+++ src/partials/CrowdSorcerer.js
@@ -0,0 +1,33 @@
+import React, { lazy, Suspense } from 'react'
+
+import 'crowdsorcerer/dist/app.css'
+import withSimpleErrorBoundary from '../util/withSimpleErrorBoundary'
+const CrowdSorcerer = lazy(() => import('crowdsorcerer'))
+
+class CrowdSorcererWrapper extends React.Component {
+  state = {
+    render: false,
+  }
+
+  componentDidMount() {
+    this.setState({ render: true })
+  }
+
+  render() {
+    if (!this.state.render) {
+      return <div>Loading...</div>
+    }
+    const { input } = this.props
+    const assignmentId = this.props.assignmentid
+    const exercises = this.props.exercisecount
+    const review = this.props.peerreview
+
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <CrowdSorcerer assignmentId={assignmentId} exercises={exercises} review={review} />
+      </Suspense>
+    )
+  }
+}
+
+export default withSimpleErrorBoundary(CrowdSorcererWrapper)
