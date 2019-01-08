f9585cfb707ce2651674676729d2390f53b6a300
Antti Leinonen <antti.leinonen@helsinki.fi>
2019-01-08 12:57:43 +0200

    Add a notification that we're fixing technical difficulties

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,11 @@ import {
   TableRow,
 } from "@material-ui/core"
 import Container from "../components/Container"
+import Notification from "../components/Notification"
 
 const IndexPage = () => (
   <Layout>
+    <Notification />
     <Banner />
     <Container>
       <section id="yleistä">
