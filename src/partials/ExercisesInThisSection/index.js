fceeb7f9d914a0aa2c9d52a7f1a1ecc9040b0451
Henrik Nygren <nygrenh@gmail.com>
2019-01-12 19:00:57 +0200

    Refactor exercises in this section

--- src/partials/ExercisesInThisSection/index.js
+++ src/partials/ExercisesInThisSection/index.js
@@ -1,7 +1,4 @@
 import React from "react"
-import PagesContext from "../../contexes/PagesContext"
-import { nthIndex } from "../../util/strings"
-import styled from "styled-components"
 import ExpansionPanel from "@material-ui/core/ExpansionPanel"
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary"
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails"
@@ -9,57 +6,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAngleDown as icon } from "@fortawesome/free-solid-svg-icons"
 
 import withSimpleErrorBoundary from "../../util/withSimpleErrorBoundary"
-import ExerciseSummary from "./ExerciseSummary"
-import { fetchManyQuizDetails } from "../../services/quiznator"
-import { flatten } from "../../util/arrays"
-
-const Title = styled.div`
-  margin-bottom: 0.5em;
-  color: rgba(0, 0, 0, 0.87);
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-  font-weight: bold;
-  font-size: 1.25rem;
-  letter-spacing: 0em;
-`
+import ExerciseList from "./ExerciseList"
 
 class ExercisesInThisSection extends React.Component {
-  static contextType = PagesContext
-
   state = {
     render: false,
-    sectionPages: null,
-    quizIdToTitle: null,
   }
 
-  async componentDidMount() {
-    const value = this.context
-    const currentPath = value.current.path
-    let sectionPath = currentPath
-    const sectionSeparator = nthIndex(currentPath, "/", 2)
-    if (sectionSeparator !== -1) {
-      sectionPath = currentPath.substr(0, sectionSeparator)
-    }
-
-    const sectionPages = value.all
-      .filter(o => o.path.startsWith(`${sectionPath}/`))
-      .sort((a, b) => {
-        a = a.path.toLowerCase()
-        b = b.path.toLowerCase()
-
-        return a > b ? 1 : b > a ? -1 : 0
-      })
-
-    const allExercises = flatten(sectionPages.map(page => page.exercises))
-    const quizIds = allExercises
-      .filter(o => o.type === "quiznator")
-      .map(o => o.id)
-    const quizDetails = await fetchManyQuizDetails(quizIds)
-    const quizIdToTitle = {}
-    quizDetails.forEach(o => {
-      quizIdToTitle[o._id] = o.title
-    })
-    this.setState({ sectionPages, quizIdToTitle, render: true })
+  componentDidMount() {
+    this.setState({ render: true })
   }
+
   render() {
     if (!this.state.render) {
       return <div>Loading...</div>
@@ -70,28 +27,7 @@ class ExercisesInThisSection extends React.Component {
           Lista osan tehtävistä
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <div>
-            {this.state.sectionPages &&
-              this.state.sectionPages.map((page, i) => (
-                <div key={page.title}>
-                  <Title>
-                    {i + 1}. {page.title}
-                  </Title>
-                  <div>
-                    {page.exercises.map((exercise, i2) => {
-                      return (
-                        <ExerciseSummary
-                          index={i2}
-                          exercise={exercise}
-                          key={exercise.id}
-                          quizIdToTitle={this.state.quizIdToTitle}
-                        />
-                      )
-                    })}
-                  </div>
-                </div>
-              ))}
-          </div>
+          <ExerciseList />
         </ExpansionPanelDetails>
       </ExpansionPanel>
     )
