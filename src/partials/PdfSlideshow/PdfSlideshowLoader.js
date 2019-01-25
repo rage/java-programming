17bbb4e927372096b89b6d1a5010d221f142d894
Henrik Nygren <nygrenh@gmail.com>
2019-01-25 11:27:57 +0200

    Add analytics to pdf slideshow

--- src/partials/PdfSlideshow/PdfSlideshowLoader.js
+++ src/partials/PdfSlideshow/PdfSlideshowLoader.js
@@ -1,7 +1,31 @@
 import React from "react"
 import PdfSlideshow, { setPdfJsWorkerPath } from "pdf-slideshow"
 import { withPrefix } from "gatsby"
+import ReduxActionAnalytics from "redux-action-analytics"
+import { canDoResearch } from "../../services/moocfi"
+import * as storejs from "store"
 
 setPdfJsWorkerPath(withPrefix("/pdf.worker.min.js"))
 
-export default props => <PdfSlideshow {...props} />
+export default props => {
+  const middleware = []
+  const analytics = new ReduxActionAnalytics(
+    "https://usage.testmycode.io/api/v0/data",
+    "pdf-slideshow",
+    props.pdfLocation,
+    10000,
+    () => {
+      const user = storejs.get("tmc.user")
+      if (user === undefined) {
+        return {}
+      }
+      return {
+        username: user.username,
+      }
+    },
+  )
+  if (canDoResearch()) {
+    middleware.push(analytics.getMiddleware())
+  }
+  return <PdfSlideshow {...props} middleware={middleware} />
+}
