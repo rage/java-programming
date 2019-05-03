3ef3e2de684e5257b070ce019adc66e0d783dce8
avihavai <avihavai@lx8-500-011.pc.helsinki.fi>
2019-05-03 15:38:28 +0300

    kesän aikataulu

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,24 +35,19 @@ const IndexPage = () => (
           opintopistettä (5+5).
         </p>
         <p>
-          Ohjelmoinnin MOOC tarjoaa mahdollisuuden opinto-oikeuteen
-          tietojenkäsittelytieteen opintoihin Helsingin yliopistolla. Mikäli
-          haet kurssin kautta opinto-oikeutta, lue kaikki opinto-oikeuden
-          hakemiseen liittyvät tiedot huolellisesti. Esimerkiksi tehtävien
-          aikarajoista EI voida joustaa lainkaan. Ehdot opinto-oikeuden
-          saamiseksi päivittyvät lähempänä yhteishakua. Lisätietoa
-          opinto-oikeudesta saat{" "}
-          <GatsbyLink to="/opinto-oikeus">täältä</GatsbyLink>.
-        </p>
-        <p>
           <b>Kurssi on käynnissä!</b> Luo tunnus tai kirjaudu sisään, jonka
           jälkeen valitse <Link to="/osa-1">Osa 1</Link> navigaatiosta.
         </p>
       </section>
 
       <section id="kurssin-kaksi-versiota">
-        <h2>Kurssin kaksi versiota</h2>
-
+        <h2>Sisältö ja kurssin versiot</h2>
+        <p>
+          Kurssi koostuu 14 tehtäväsarjasta. Kunkin tehtäväsarjan tekemiseen
+          kannattaa varata aikaa noin 5-20 tuntia. Varaamme oikeuden aikataulun
+          ja aiheiden muutoksiin, kuitenkin niin, että jokaista tehtäväsarjaa on
+          mahdollista suorittaa vähintään kahden viikon ajan.
+        </p>
         <p>
           Kurssista on kaksi versiota: aikataulutettu versio, jossa on
           viikoittaiset deadlinet ja aikatauluton versio, jossa tehtäviä voi
@@ -63,24 +58,114 @@ const IndexPage = () => (
           kuitenkin että aikatauluttomasta versiosta ei voi hakea
           opinto-oikeutta.
         </p>
+        <p>
+          Kurssin osat 1-7 vastaavat kurssia Ohjelmoinnin perusteet. Osat 8-14
+          vastaavat kurssia Ohjelmoinnin jatkokurssi.
+        </p>
       </section>
 
       <section id="sisältö-ja-aikataulu">
-        <h2>Sisältö ja aikataulu</h2>
+        <h3>Aikataulu - kesä 2019</h3>
 
         <p>
-          Kurssi koostuu 14 tehtäväsarjasta. Kunkin tehtäväsarjan tekemiseen
-          kannattaa varata aikaa noin 5-20 tuntia. Varaamme oikeuden aikataulun
-          ja aiheiden muutoksiin, kuitenkin niin, että jokaista tehtäväsarjaa on
-          mahdollista suorittaa vähintään kahden viikon ajan.
+          Kesän 2019 aikataulutetussa Ohjelmoinnin MOOCissa kurssimateriaali on
+          kokonaisuudessaan saatavilla kurssin alusta lähtien. Tämä mahdollistaa
+          kurssin tekemisen nopeammalla tahdilla, mikäli niin haluaa tehdä.
+          Normaalissa kesän aikataulussa kunkin osan tekemiseen on noin viikko.
+          Ensimmäisen osan tehtävät tulee palauttaa 13.5. klo 23:59:00 mennessä.
         </p>
+
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Aikataulu</TableCell>
+              <TableCell>Deadline</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Osa 1</TableCell>
+              <TableCell>13.5.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 2</TableCell>
+              <TableCell>20.5.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 3</TableCell>
+              <TableCell>27.5.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 4</TableCell>
+              <TableCell>3.6.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 5</TableCell>
+              <TableCell>10.6.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 6</TableCell>
+              <TableCell>17.6.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 7</TableCell>
+              <TableCell>24.6.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 8</TableCell>
+              <TableCell>8.7.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 9</TableCell>
+              <TableCell>15.7.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 10</TableCell>
+              <TableCell>22.7.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 11</TableCell>
+              <TableCell>29.7.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 12</TableCell>
+              <TableCell>5.8.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 13</TableCell>
+              <TableCell>12.8.2019</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Osa 14</TableCell>
+              <TableCell>19.8.2019</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
 
         <p>
-          Kurssin sisältö tarkentuu kurssin edetessä. Tietoa kurssin
-          aikatauluista sekä sisällöstä lisätään tälle sivulle kurssin
-          aloituksen lähestyessä.
+          <b>
+            Määräajat ovat aina maanantaisin. Määräaikojen kellonajat ovat aina
+            23:59:00 – osan 1 tehtävät tulee palauttaa siis viimeistään
+            13.5.2019 klo 23:59:00. Virallisena määräaikana käytetään
+            tehtäväpalvelimen kelloa, joka on Suomen ajassa. Huomaa, että oman
+            koneesi kello voi olla jäljessä tai edellä, joten älä jätä tehtävien
+            tekemistä ja palauttamista viime hetkeen.
+          </b>
         </p>
+      </section>
 
+      <section id="sisältö-ja-aikataulu-kevat-2019">
+        <h3>Aikataulu - kevät 2019</h3>
+        <p>
+          Keväisin tarjottava Ohjelmoinnin MOOC tarjoaa mahdollisuuden
+          opinto-oikeuteen tietojenkäsittelytieteen opintoihin Helsingin
+          yliopistolla. Mikäli haet kurssin kautta opinto-oikeutta, lue kaikki
+          opinto-oikeuden hakemiseen liittyvät tiedot huolellisesti. Esimerkiksi
+          tehtävien aikarajoista EI voida joustaa lainkaan. Ehdot
+          opinto-oikeuden saamiseksi päivittyvät lähempänä yhteishakua.
+          Lisätietoa opinto-oikeudesta saat{" "}
+          <GatsbyLink to="/opinto-oikeus">täältä</GatsbyLink>.
+        </p>
         <Table>
           <TableHead>
             <TableRow>
