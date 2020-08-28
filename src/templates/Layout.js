f31b7214b5a6ff82f76bef53e1bfa171757aa8a8
Rowan Hoogervorst <hoogervorst@ese.eur.nl>
2020-08-28 09:06:54 +0200

    Update helmet

--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -120,12 +120,12 @@ class Layout extends React.Component {
                     {
                       name: "description",
                       content:
-                        "Helsingin yliopiston kaikille avoin ja ilmainen ohjelmoinnin perusteet opettava verkkokurssi. Kurssilla perehdytään nykyaikaisen ohjelmoinnin perusideoihin sekä ohjelmoinnissa käytettävien työvälineiden lisäksi algoritmien laatimiseen. Kurssille osallistuminen ei vaadi ennakkotietoja ohjelmoinnista.",
+			    "Course webpage for the Introduction to Programming Course of the Erasmus School of Economics.",
                     },
                     {
                       name: "keywords",
                       content:
-                        "ohjelmointi, java, programming, CS1, MOOC, 2019, ohjelmointikurssi, avoin, ilmainen, helsingin yliopisto",
+                        "java, programming, ESE, EUR, course",
                     },
                   ]}
                 />
