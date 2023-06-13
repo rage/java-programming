23227af8a08181c9749686ad334ff2e2f7aba3de
Antti Leinonen <antti.leinonen@helsinki.fi>
2023-06-14 02:56:01 +0300

    Use @apollo/client@3.6.2 and graphql@15.8.0

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 import { red, blue } from "@material-ui/core/colors"
-import { createMuiTheme } from "@material-ui/core/styles"
+import { createTheme } from "@material-ui/core/styles"
 
 // A custom theme for this app
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       light: blue[300],
